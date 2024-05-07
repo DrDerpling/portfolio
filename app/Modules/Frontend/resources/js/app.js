f7127235cd8473a6a5d354750d7615e6104a0f19
Dennis Lindeboom <dlindeboom19@outlook.com>
2024-05-08 00:03:17 +0200

    WIP bases for tabs on top

--- app/Modules/Frontend/resources/js/app.js
+++ app/Modules/Frontend/resources/js/app.js
@@ -1,21 +1,5 @@
 import './bootstrap.js';
 
-// Select all elements that have the attribute 'data-parent-menu-item'
-const menuItems = document.querySelectorAll('[data-parent-menu-item]');
-
-const menuToggleCallback = (event) => {
-    const menuItems = event.target.parentElement.nextElementSibling.querySelectorAll('[data-menu-item]');
-    const chevrons = event.target.parentElement.querySelectorAll('.chevron');
-
-    menuItems.forEach(item => {
-        item.classList.toggle('hidden');
-    })
-
-    chevrons.forEach(chevron => {
-        chevron.classList.toggle('hidden');
-    });
-};
-
 const calculateLineNumbers = () => {
     const lineNumbersDivs = document.querySelectorAll('.line-numbers');
 
@@ -25,7 +9,7 @@ const calculateLineNumbers = () => {
 const calculateLineNumbersCallback = (element) => {
     element.innerHTML = '';
 
-    const offset =  100;
+    const offset = 100;
     const containerHeight = element.parentElement.clientHeight + offset - parseInt(window.getComputedStyle(element.parentElement).paddingTop) - parseInt(window.getComputedStyle(element.parentElement).paddingBottom) - parseInt(element.offsetTop);
     const lineHeight = parseInt(element.getAttribute('data-line-height'), 10);
 
@@ -43,10 +27,6 @@ const calculateLineNumbersCallback = (element) => {
     }
 }
 
-menuItems.forEach(item => {
-    item.addEventListener('click', menuToggleCallback);
-});
-
 document.addEventListener("DOMContentLoaded", calculateLineNumbers);
 window.addEventListener("resize", calculateLineNumbers);
-
+document.addEventListener("livewire:navigated", calculateLineNumbers);
--- app/Modules/Frontend/resources/views/layouts/code-editor.blade.php
+++ app/Modules/Frontend/resources/views/layouts/code-editor.blade.php
@@ -12,8 +12,22 @@
         <div class="flex bg-deep-gray text-white lg:h-screen">
             <x-navbar></x-navbar>
             <div class="flex flex-col flex-1 h-screen">
-                <div class="flex items-start justify-center flex-col h-16 min-h-16 bg-charcoal">
-
+                <div x-data="{
+                    links: [],
+                    init() {
+                        this.links = JSON.parse(localStorage.getItem('visitedLinks')) || [];
+                    },
+                    removeItem(index) {
+                        this.links.splice(index, 1);
+                        localStorage.setItem('visitedLinks', JSON.stringify(this.links));
+                    }
+                }" x-init="init" class="flex items-end flex-end h-16 min-h-16 bg-charcoal" >
+                    <template x-for="(link, index) in links">
+                        <div class="px-2 py-1 border-t-2 border-t border-t-bright-cyan border-x border-x-slate">
+                            <a  x-text="link.title" :href="link.link" class="text-white"></a>
+                            <button x-on:click="removeItem(index)">x</button>
+                        </div>
+                    </template>
                 </div>
                 <div class="flex parent-container h-screen overflow-y-auto">
                    <x-line-numbers line-height="25" height="90" />
@@ -24,6 +38,7 @@
             </div>
 
         </div>
+
         @vite('app/Modules/Frontend/resources/js/app.js')
         @livewireScripts
     </body>
