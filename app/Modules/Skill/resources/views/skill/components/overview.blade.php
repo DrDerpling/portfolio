a64244daf7ee0dfb0fb8a34149f8981468a76107
Dennis Lindeboom <dlindeboom19@outlook.com>
2024-05-11 21:37:24 +0200

    IMP responsiveness

--- app/Modules/Skill/resources/views/skill/components/overview.blade.php
+++ app/Modules/Skill/resources/views/skill/components/overview.blade.php
@@ -2,14 +2,14 @@
     /** @var \Illuminate\Support\Collection $groupedSkills */
 @endphp
 
-<div class="p-4 w-full text-primary bg-primary-darker border-primary-darkest border flex justify-center">
-    <div class="bg-primary text-primary-content p-6 max-w-4xl mx-auto">
+<div class="w-full text-primary bg-primary-darker border-primary-darkest border flex justify-center">
+    <div class="bg-primary text-primary-content p-6 w-full lg:max-w-4xl mx-auto">
         <h2 class="text-2xl font-bold mb-4">Skills & Expertise</h2>
         @foreach($groupedSkills as $groups => $skills)
             <div class="mb-4">
                 <h3 class="font-semibold text-lg mb-2">{{ $groups }}</h3>
                 <!-- Responsive grid setup: 2 columns on mobile, 5 on desktop -->
-                <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+                <div class="grid grid-cols-2  md:grid-cols-4  lg:grid-cols-6 gap-2 md:gap-4">
                     @foreach($skills as $skill)
                         <div class="flex flex-col justify-between text-center shadow-lg">
                             <div class="bg-primary-darker p-2">
