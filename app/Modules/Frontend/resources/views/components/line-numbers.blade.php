077d66ddf0e51cde71015984b89003c64c26bcbf
Dennis Lindeboom <dlindeboom19@outlook.com>
2024-05-18 16:47:08 +0200

    FIX card height

--- app/Modules/Frontend/resources/views/components/line-numbers.blade.php
+++ app/Modules/Frontend/resources/views/components/line-numbers.blade.php
@@ -4,6 +4,6 @@
 @endphp
 
 <div data-line-height="{{ $lineHeight }}"
-     class="line-numbers text-primary-darkest absolute overflow-hidden">
+     class="line-numbers text-primary-darkest absolute text-right overflow-hidden">
     <!-- Contains line number, which are divs so they go below each other -->
 </div>
--- app/Modules/Project/resources/views/project/components/card.blade.php
+++ app/Modules/Project/resources/views/project/components/card.blade.php
@@ -10,14 +10,19 @@
      */
 @endphp
 
-<div class="mx-auto bg-primary-darkest">
-    <img src="{{ $project['image'] }}" alt="Project Gezond App Interface"
-         class="h-48 object-contain w-full">
-    <div class="p-4 bg-primary-darker">
-        <h2 class="font-semibold text-xl text-primary-lighter mb-2"> {{  $project['title'] }}</h2>
-        <p class="text-primary-lighter text-base mb-4 overflow-hidden">
-            {{ Str::limit($project['description'], 300)}}
-        </p>
+<div class="bg-primary-darker flex flex-col h-full">
+    <div>
+        <img src="{{ $project['image'] }}" alt="Project Gezond App Interface"
+             class="h-48 object-contain w-full bg-primary-darkest">
+        <div class="px-4">
+            <h2 class="font-semibold text-xl text-primary-lighter mb-2">{{ $project['title'] }}</h2>
+            <p class="text-primary-lighter text-base mb-4 overflow-hidden">
+                {{ Str::limit($project['description'], 300) }}
+            </p>
+        </div>
+    </div>
+
+    <div class="mt-auto px-4">
         <div class="flex flex-wrap gap-1 items-start mb-4 overflow-hidden">
             @foreach($project['badges'] as $badge)
                 <span class="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#{{ $badge }}</span>
@@ -31,3 +36,4 @@
         </div>
     </div>
 </div>
+
